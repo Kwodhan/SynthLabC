044242ae56ba8fdb7e3fb6e4ed925304b523095b
chakib <benkebir.chakib@gmail.com>
2018-01-31 11:49:33 +0100

    add jsyn

--- src/main/java/com/istic/Controller.java
+++ src/main/java/com/istic/Controller.java
@@ -2,12 +2,15 @@ package com.istic;
 
 import javafx.fxml.Initializable;
 
+import javax.sound.midi.Synthesizer;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
+        
+
 
     }
 
