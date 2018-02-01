bada1ee322547cf51d97cd487a26484abc02520a
m1info <inaduor@gmail.com>
2018-02-01 10:32:21 +0100

    implementation du module de sortie

--- src/main/java/com/istic/OutMod.java
+++ src/main/java/com/istic/OutMod.java
@@ -3,6 +3,27 @@ package com.istic;
 import com.jsyn.unitgen.LineOut;
 
 public class OutMod extends LineOut implements Module {
+	private double attenuation = 0;
+	private boolean mute = false;
 
+	public double getAttenuation() {
+		return attenuation;
+	}
+
+	public void setAttenuation(double attenuation) {
+		this.attenuation = attenuation;
+	}
+
+	public boolean isMute() {
+		return mute;
+	}
+
+	public void setOnMute() {
+		this.mute = true;
+	}
+	public void setOffMute() {
+		this.mute = false;
+	}	
+	
 
 }
