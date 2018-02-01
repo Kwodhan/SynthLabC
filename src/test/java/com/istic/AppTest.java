e6d3902193ea76bfc99b5a47e8d5bbf9fe14eb04
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2018-02-01 11:45:35 +0100

    Add simple tests

--- src/test/java/com/istic/AppTest.java
+++ src/test/java/com/istic/AppTest.java
@@ -1,38 +1,65 @@
 package com.istic;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.istic.cable.Cable;
+import com.jsyn.JSyn;
+import com.jsyn.Synthesizer;
+import com.jsyn.unitgen.SquareOscillatorBL;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    private Synthesizer synth;
+
+    @Before
+    public void initSynth() {
+        this.synth = JSyn.createSynthesizer();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testSimpleSynth() {
+        assertNotNull(this.synth);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testSynthWithSquareOscillatorBL() {
+        SquareOscillatorBL osc = new SquareOscillatorBL();
+        this.synth.add(osc);
+        assertNotNull(osc);
+    }
+
+    @Test
+    public void testCreateVCO() {
+        VCO vco = new VCO(this.synth);
+        assertNotNull(vco);
+    }
+
+    @Test
+    public void testCreateOutMod() {
+        OutMod outMod = new OutMod(this.synth);
+        assertNotNull(outMod);
+    }
+
+    @Test
+    public void testCreateCable() {
+        OutMod lineOut = new OutMod(this.synth);
+        VCO vco = new VCO(this.synth);
+
+        Cable cable = new Cable(vco.getPortOutput(),lineOut.getPortInput());
+        assertNotNull(cable);
+
+        cable.connect();
+
+        assertTrue(cable.getPortOne().isConnected());
+        assertTrue(cable.getPortTwo().isConnected());
+
+        cable.disconnect();
+
+        assertFalse(cable.getPortOne().isConnected());
+        assertFalse(cable.getPortTwo().isConnected());
     }
 }
