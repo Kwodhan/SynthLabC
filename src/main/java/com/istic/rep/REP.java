c6e10d3d79dd11b7ba64d2da93d44a5d9429fdd4
kristof66635 <christopheplanchais@gmail.com>
2018-02-06 15:30:52 +0100

    rep mod2

--- src/main/java/com/istic/rep/REP.java
+++ src/main/java/com/istic/rep/REP.java
@@ -3,6 +3,7 @@ package com.istic.rep;
 import com.istic.port.PortFm;
 import com.istic.port.PortOutput;
 import com.istic.vca.ReglageVCA;
+import com.jsyn.ports.UnitInputPort;
 import com.jsyn.ports.UnitOutputPort;
 import com.jsyn.unitgen.Circuit;
 import com.jsyn.unitgen.PassThrough;
@@ -13,9 +14,11 @@ public class REP  extends Circuit{
 	   /**
      * Port de sortie du VCO
      */
-    private UnitOutputPort out;
-    private UnitOscillator oscillator ;
-
+    private UnitInputPort in;
+	private UnitOutputPort out1;
+	private UnitOutputPort out2;
+	private UnitOutputPort out3;
+ 
 
     /**
      * reglage ao, am
@@ -27,16 +30,28 @@ public class REP  extends Circuit{
      public REP() {
 
         add(reglageREP = new ReglageREP());
-        //add(oscillator);
-        //addPort(oscillator.getOutput());
-        //reglageREP.getOut().connect(oscillator.amplitude);
+         addPortAlias(out1 = reglageREP.getOut1(), "com/istic/out1");
+        addPortAlias(out2 = reglageREP.getOut2(), "com/istic/out2");
+        addPortAlias(out3 = reglageREP.getOut3(), "com/istic/out3");
+        addPortAlias(in =reglageREP.getInput(),"in");
+ 
+         //addPort(oscillator.getOutput());
+        //reglageVCA.getOut().connect(oscillator.amplitude);
 
 
     }
-    public PortOutput getOutput() {
+     public PortOutput getOutput1() {
 
-        return new PortOutput(out);
-    }
+         return new PortOutput(out1);
+     }
+     public PortOutput getOutput2() {
+
+         return new PortOutput(out2);
+     }
+     public PortOutput getOutput3() {
+
+         return new PortOutput(out3);
+     }
  
 	 
 }
