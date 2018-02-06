7ce4aedf3b96e73fa9dfce0bb6c8e898ba800cc6
kristof66635 <christopheplanchais@gmail.com>
2018-02-06 14:14:36 +0100

    multicable2

--- src/main/java/com/istic/cable/MultiCable.java
+++ src/main/java/com/istic/cable/MultiCable.java
@@ -7,6 +7,56 @@
 
 package com.istic.cable;
 
-public class MultiCable {
+import java.util.ArrayList;
 
+import com.istic.port.Port;
+
+public class MultiCable {
+	   Port portOne;
+	    Port portTwo;
+ArrayList<Port> ports_out= new ArrayList<>();
+
+	    public MultiCable(Port portOne, Port portTwo) {
+	        this.portOne = portOne;
+	        this.portTwo = portTwo;
+	        this.ports_out.add(portTwo);
+
+	    }
+public void add_output_port ( Port p ) {
+    this.ports_out.add( p );
+
+}
+	    public Port getPortOne() {
+	        return portOne;
+	    }
+
+	    public Port getPortTwo() {
+	        return portTwo;
+	    }
+	    public Port getPortOut(int i) {
+	        return this.ports_out.get(i);
+	    }
+
+	    public boolean  connect() {
+	        if(this.portTwo.accept(this.portOne.getVisitorConnectPort())){
+	            this.portOne.setConnected(true);
+	            for (int i =0; i< this.ports_out.size(); i++) {
+	            this.ports_out.get(i).setConnected(true);
+	            }
+	            return true;
+	        }
+	        return false;
+
+
+	    }
+	    public void disconnect() {
+	        this.portOne.setConnected(false);
+	        for (int i =0; i< this.ports_out.size(); i++) {
+	            this.ports_out.get(i).setConnected(false);
+	            }
+ 	        this.portOne.disconnect();
+	        for (int i =0; i< this.ports_out.size(); i++) {
+	            this.ports_out.get(i).disconnect( );
+	            }
+ 	    }
 }
