d5082adc1d748aa22e1efb2462c2a3a1f7ea7b5b
kristof66635 <christopheplanchais@gmail.com>
2018-02-06 10:56:39 +0100

    class mod eg ajoutée

---
+++ src/main/java/com/istic/eg/EG.java
@@ -0,0 +1,68 @@
+package com.istic.eg;
+
+import java.nio.channels.GatheringByteChannel;
+
+import com.istic.port.PortGate;
+import com.istic.port.PortOutput;
+import com.jsyn.JSyn;
+import com.jsyn.Synthesizer;
+import com.jsyn.unitgen.EnvelopeDAHDSR;
+import com.jsyn.unitgen.LineOut;
+import com.jsyn.unitgen.SquareOscillator;
+
+public class EG {
+	EnvelopeDAHDSR env = new EnvelopeDAHDSR();
+
+	public PortGate getGateInput() {
+		return new PortGate(env.input);
+	}
+
+	public PortOutput getOutputPort() {
+		return new PortOutput(env.output);
+	}
+
+	public double getAttack() {
+		return this.env.attack.getValue();
+	}
+
+	public void setAttack(double attack) {
+		this.env.attack.set(attack);
+	}
+
+	public double getDecay() {
+		return this.env.decay.getValue();
+	}
+
+	public void setDecay(double decay) {
+		this.env.decay.set(decay);
+
+	}
+
+	public double getSustain() {
+		return this.env.sustain.getValue();
+	}
+
+	public void setSustain(double sustain) {
+		this.env.sustain.set(sustain);
+
+	}
+
+	public double getRelease() {
+		return this.env.release.getValue();
+	}
+
+	public void setRelease(double release) {
+		this.env.release.set(release);
+
+	}
+
+	public static void main() {
+		Synthesizer synth = JSyn.createSynthesizer();
+		SquareOscillator gatingOsc;
+		synth.add(gatingOsc = new SquareOscillator());
+		EnvelopeDAHDSR dahdsr;
+		synth.add(dahdsr = new EnvelopeDAHDSR());
+		LineOut lineOut;
+		synth.add(lineOut = new LineOut());
+	}
+}
