a285e688b3acad085774bcd9cca20e7bab993058
kristof66635 <christopheplanchais@gmail.com>
2018-02-06 16:25:38 +0100

    egctrl

--- src/main/java/com/istic/eg/EG.java
+++ src/main/java/com/istic/eg/EG.java
@@ -4,53 +4,38 @@ import com.istic.port.PortGate;
 import com.istic.port.PortOutput;
 import com.jsyn.unitgen.EnvelopeDAHDSR;
 
-public class EG {
-	EnvelopeDAHDSR env = new EnvelopeDAHDSR();
-	
-	public EnvelopeDAHDSR getEnv() {
-		return this.env;
-	}
+public class EG extends EnvelopeDAHDSR {
+//	EnvelopeDAHDSR env = new EnvelopeDAHDSR();
+//	
+//	public EnvelopeDAHDSR getEnv() {
+//		return this.env;
+//	}
 
 	public PortGate getGateInput() {
-		return new PortGate(env.input);
+		return new PortGate(this.input);
 	}
 
 	public PortOutput getOutputPort() {
-		return new PortOutput(env.output);
+		return new PortOutput(this.output);
 	}
-
-	public double getAttack() {
-		return this.env.attack.getValue();
-	}
-
+ 
 	public void setAttack(double attack) {
-		this.env.attack.set(attack);
+		this.attack.set(attack);
 	}
-
-	public double getDecay() {
-		return this.env.decay.getValue();
-	}
+ 
 
 	public void setDecay(double decay) {
-		this.env.decay.set(decay);
+		this.decay.set(decay);
 
 	}
-
-	public double getSustain() {
-		return this.env.sustain.getValue();
-	}
+ 
 
 	public void setSustain(double sustain) {
-		this.env.sustain.set(sustain);
+		this.sustain.set(sustain);
 
-	}
-
-	public double getRelease() {
-		return this.env.release.getValue();
-	}
-
+	} 
 	public void setRelease(double release) {
-		this.env.release.set(release);
+		this.release.set(release);
 
 	}
 
--- src/main/java/com/istic/modulesController/EGModuleController.java
+++ src/main/java/com/istic/modulesController/EGModuleController.java
@@ -1,13 +1,35 @@
 package com.istic.modulesController;
 
+import com.istic.eg.EG;
 import com.istic.port.Port;
+import com.istic.port.PortOutput;
+import com.istic.vco.VCO;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class EGModuleController extends ModuleController implements Initializable {
+	
+	protected EG eg;
+    @FXML
+    ImageView outPort;
+    @FXML
+    ImageView gateport;
+    @FXML
+    Slider attackSlider;
+    @FXML
+    Slider decaySlider;
+    @FXML
+    Slider sustainSlider;
+    @FXML
+    Slider releaseSlider;
     /**
      * Called to initialize a controller after its root element has been
      * completely processed.
@@ -21,8 +43,38 @@ public class EGModuleController extends ModuleController implements Initializabl
 
     }
 
-    @Override
     public Port getCurrentPort() {
+        if (currentPort == 0) {
+            return eg.getOutputPort();
+        } else if (currentPort == 1) {
+            return eg.getGateInput();
+        }
         return null;
+    }
+    public PortOutput getOutPort() {
+
+        return this.eg.getOutputPort();
+
+    }
+    public void init(Controller controller) {
+        super.init(controller);
+        this.eg = new EG();
+        this.controller.getSynth().add(eg);
+
+    }
+    
+    public void connectGatePort() {
+        currentPort = 1;
+        getLayout(gateport);
+        super.connect();
+    }
+
+    /**
+     * Connecting the outPort to draw the cable
+     */
+    public void connectOutPort() {
+        currentPort = 0;
+        getLayout(outPort);
+        super.connect();
     }
 }
