fce2d0bb1116023cc2a848672651d887e0b22549
kristof66635 <christopheplanchais@gmail.com>
2018-02-13 14:39:20 +0100

    kb4

--- src/main/java/com/istic/keyboard/KBListener.java
+++ src/main/java/com/istic/keyboard/KBListener.java
@@ -1,19 +1,65 @@
 package com.istic.keyboard;
+import javafx.scene.Scene;
+
 import javafx.scene.input.KeyEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode; 
 
 public class KBListener implements EventHandler<KeyEvent> {
-
+	ReglageKB reglageKB;
+	public KBListener(ReglageKB rkb) {
+		this.reglageKB= rkb;
+	}
+	public void add_listener (Scene sc) {
+		sc.setOnKeyPressed(this);
+	}
 	@Override
 	public void handle(KeyEvent event) {
+		if (event.getEventType() == KeyEvent.KEY_PRESSED) {
 		   switch (event.getCode()) {
-           case UP:     System.out.println("Hello World"); break;
-           case DOWN:   System.out.println("Hello World");; break;
-           case LEFT:   System.out.println("Hello World");break;
-           case RIGHT:  System.out.println("Hello World"); break;
-           case SHIFT:  System.out.println("Hello World"); break;
-       }		
+		   case Q:   reglageKB.onpressDO (); break;
+		   case S:   reglageKB.onpressRE (); break;
+		   case D:   reglageKB.onpressMI (); break;
+		   case F:   reglageKB.onpressFA (); break;
+		   case G:   reglageKB.onpressSOL (); break;
+		   case H:   reglageKB.onpressLA(); break;
+		   case J:   reglageKB.onpressSI (); break;
+		   case K:   reglageKB.onpressDO2 (); break;
+
+		   
+		   case Z:   reglageKB.onpressDOd (); break;
+		   case E:   reglageKB.onpressREd (); break;
+		   case T:   reglageKB.onpressFAd (); break;
+		   case Y:   reglageKB.onpressSOLd (); break;
+		   case U:   reglageKB.onpressLAd (); break;
+
+		   case W:   reglageKB.onpressOctaveUP (); break;
+		   case X:   reglageKB.onpressOctaveDOWN (); break;
+            }		
+		}
+		else if ( event.getEventType() == KeyEvent.KEY_RELEASED) {
+			   switch (event.getCode()) {
+			   case Q:   reglageKB.onreleaseDO (); break;
+			   case S:   reglageKB.onreleaseRE (); break;
+			   case D:   reglageKB.onreleaseMI (); break;
+			   case F:   reglageKB.onreleaseFA (); break;
+			   case G:   reglageKB.onreleaseSOL (); break;
+			   case H:   reglageKB.onreleaseLA(); break;
+			   case J:   reglageKB.onreleaseSI (); break;
+			   case K:   reglageKB.onreleaseDO2 (); break;
+
+			   
+			   case Z:   reglageKB.onreleaseDOd (); break;
+			   case E:   reglageKB.onreleaseREd (); break;
+			   case T:   reglageKB.onreleaseFAd (); break;
+			   case Y:   reglageKB.onreleaseSOLd (); break;
+			   case U:   reglageKB.onreleaseLAd (); break;
+
+			   case W:   reglageKB.onreleaseOctaveUP (); break;
+			   case X:   reglageKB.onreleaseOctaveDOWN (); break;
+	            }
+			
+		}
 	}
 
 }
--- src/main/java/com/istic/keyboard/ReglageKB.java
+++ src/main/java/com/istic/keyboard/ReglageKB.java
@@ -6,12 +6,190 @@ import com.jsyn.ports.UnitOutputPort;
 import com.jsyn.unitgen.UnitGenerator;
 
 public class ReglageKB extends UnitGenerator{
+	int octave_min=0;
+	int octave=0;
+	int octave_max=0;
+boolean notes[]= new boolean[13];
+double notes_hz[]= new double[13];
+
+public   ReglageKB() {
+	for (boolean n : notes) {
+		n=false;
+		}
+	for (double n : notes_hz) {
+		n=0.0d;
+		}
+	octave_min=0;
+	octave =0;
+	octave_max=0;
+}
 
 	@Override
 	public void generate(int arg0, int arg1) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	void add_frequency(int note ) {
+		notes_hz[note]=  octave*0; //la formule...
+	}
+public void update_signal() {
+	for (int i =0;i< 13;i++) {
+		if (notes[i]== true) {
+			//notes_hz[i]; //push effect to output
+		}
+	}
+}
+private void toggle (int note) {
+	this.notes[note]=  ! this.notes[note];
+}
+	public void onpressDO() {
+		toggle(0);		
+	}
+
+
+	public void onpressRE() {
+		toggle(1);		
+		
+	}
+
+	public void onpressMI() {
+		toggle(2);		
+		
+	}
+
+	public void onpressFA() {
+		toggle(3);		
+		
+	}
+
+	public void onpressSOL() {
+		toggle(4);		
+		
+	}
+
+	public void onpressLA() {
+		toggle(5);		
+		
+	}
+
+	public void onpressSI() {
+		toggle(6);		
+		
+	}
+
+	public void onpressDO2() {
+		toggle(7);		
+		
+	}
+
+	public void onpressDOd() {
+		toggle(8);		
+		
+	}
+
+	public void onpressREd() {
+		toggle(9);		
+		
+	}
+
+	public void onpressFAd() {
+		toggle(10);		
+		
+	}
+
+	public void onpressSOLd() {
+		toggle(11);		
+		
+	}
+	public void onpressLAd() {
+		toggle(12);		
+		
+	}
+	public void onpressOctaveUP() {
+if (this.octave< this.octave_max) octave ++;		
+	}
+
+	public void onpressOctaveDOWN() {
+		if (this.octave> this.octave_min) octave --;		
+		
+	}
+
+	public void onreleaseDO() {
+		toggle(0);		
+		
+	}
+
+	public void onreleaseRE() {
+		toggle(1);		
+		
+	}
+
+	public void onreleaseMI() {
+		toggle(2);		
+		
+	}
+
+	public void onreleaseFA() {
+		toggle(3);		
+		
+	}
+
+	public void onreleaseSOL() {
+		toggle(4);		
+		
+	}
+
+	public void onreleaseLA() {
+		toggle(5);		
+		
+	}
+
+	public void onreleaseSI() {
+		toggle(6);		
+		
+	}
+
+	public void onreleaseDO2() {
+		toggle(7);		
+		
+	}
+
+	public void onreleaseDOd() {
+		toggle(8);		
+		
+	}
+
+	public void onreleaseREd() {
+		toggle(9);		
+		
+	}
+
+	public void onreleaseFAd() {
+		toggle(10);		
+		
+	}
+	public void onreleaseSOLd() {
+		toggle(11);		
+		
+	}
+
+	public void onreleaseLAd() {
+		toggle(12);		
+		
+	}
+
+	public void onreleaseOctaveUP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onreleaseOctaveDOWN() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
  
 	 
 }
