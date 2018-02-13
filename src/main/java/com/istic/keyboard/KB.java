c8d67394e79f04e0ba53361f11e22d8c484bbbb3
kristof66635 <christopheplanchais@gmail.com>
2018-02-13 10:49:40 +0100

    keayboard1

---
+++ src/main/java/com/istic/keyboard/KB.java
@@ -0,0 +1,7 @@
+package com.istic.keyboard;
+
+import com.jsyn.unitgen.Circuit;
+
+public class KB  extends Circuit {
+
+}
---
+++ src/main/java/com/istic/keyboard/ReglageKB.java
@@ -0,0 +1,13 @@
+package com.istic.keyboard;
+
+import com.jsyn.unitgen.UnitGenerator;
+
+public class ReglageKB extends UnitGenerator{
+
+	@Override
+	public void generate(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
