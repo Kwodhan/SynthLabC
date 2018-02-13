7660596867ca4a56208efd3cefb60f1c90e4a35c
kristof66635 <christopheplanchais@gmail.com>
2018-02-13 15:57:08 +0100

    kb5

--- src/main/java/com/istic/keyboard/KB.java
+++ src/main/java/com/istic/keyboard/KB.java
@@ -1,16 +1,20 @@
 package com.istic.keyboard;
 
+import java.util.ArrayList;
+
 import com.istic.port.PortGate;
 import com.istic.port.PortInput;
 import com.istic.port.PortOutput;
 import com.jsyn.ports.UnitGatePort;
 import com.jsyn.ports.UnitOutputPort;
 import com.jsyn.unitgen.Circuit;
+import com.jsyn.unitgen.SineOscillator;
+import com.jsyn.unitgen.UnitOscillator;
 
 public class KB  extends Circuit {
     private PortOutput portCv; // +1V par octave ???
     private PortGate portGate; //+5V par octave ???
-  //  private ReglageKB reglageKB;
+    ReglageKB reglageKB = new ReglageKB();
 
     public KB() {
     	this.portGate = new PortGate(new UnitGatePort ("gate"));
