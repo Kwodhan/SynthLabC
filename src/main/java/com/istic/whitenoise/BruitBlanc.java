54ac868ab1b090dab8201d2760dbf4e3d5029813
m1info <inaduor@gmail.com>
2018-02-14 09:33:57 +0100

    java doc BruitBlanc

--- src/main/java/com/istic/whitenoise/BruitBlanc.java
+++ src/main/java/com/istic/whitenoise/BruitBlanc.java
@@ -3,13 +3,24 @@ package com.istic.whitenoise;
 import com.istic.port.PortOutput;
 import com.jsyn.unitgen.WhiteNoise;
 
+/**
+ *  Classe pour la creation du bruit blanc
+ */
 public class BruitBlanc extends WhiteNoise {
 	
 	private PortOutput out;
+	
+	/**
+	 * Constructeur de la class BruitBlanc et son port de sortie
+	 */
 	public BruitBlanc(){
 		this.out = new PortOutput(this.output);
 	}
 
+	/**
+	 * getter pour le port de sortie
+	 * @return
+	 */
 	public PortOutput getOutputPort(){
 		return  out;
 	}
