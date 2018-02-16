d6a137a1e9accd95e5332cc1331b4cf7838369d8
aferey <kwodhan@gmail.com>
2018-02-16 13:06:19 +0100

    clean code WIP KeyBoard

--- src/main/java/com/istic/keyboard/ReglageKB.java
+++ src/main/java/com/istic/keyboard/ReglageKB.java
@@ -16,21 +16,21 @@ import javafx.scene.control.TextArea;
 public class ReglageKB extends UnitGenerator {
     private final int octave_min = -5;
     private final int octave_max = 4;
-    int octave = 0;
-    final double Vo = 1.0476;
-    boolean notes[] = new boolean[13];
-    double notes_hz[] = new double[13];
-    String notes_descr[] = {
+    private int octave = 0;
+    private final double Vo = 1.0476;
+    private boolean notes[] = new boolean[13];
+    private double notes_hz[] = new double[13];
+    private String notes_descr[] = {
             "DO", "DOd", "RE", "REd", "MI", "FA", "FAd", "SOL", "SOLd", "LA", "LAd", "SI", "DO2"};
-    String notes_touch[] = {
+    private String notes_touch[] = {
             "Q", "Z", "S", "E", "D", "F", "T", "G", "Y", "H", "U", "J", "K"};
-    double cur_hz = 0;
+    private double cur_hz = 0;
+
+    private boolean firsttime = true;
 
     private UnitOutputPort portGate;
     private UnitOutputPort portCv;
-    
 
-	
 
     boolean notes_playing[] = new boolean[13];
     long notes_timestamp[] = new long[13];
@@ -44,146 +44,136 @@ public class ReglageKB extends UnitGenerator {
     }
 
 
-
-
-
     /////////////////////////////////////////////////////////////
     @Override
     public void generate(int start, int limit) {
-        double[] outputsGate= portGate.getValues();
-        double[] outputsCv= portCv.getValues();
+        double[] outputsGate = portGate.getValues();
+        double[] outputsCv = portCv.getValues();
 
 
-        for(int i = start; i < limit; ++i) {
+        for (int i = start; i < limit; ++i) {
 
-            outputsGate[i] = get_gate()/5;
+            outputsGate[i] = get_gate() / Constraints.VOLT;
 
-            outputsCv[i] = get_cv()/5;
+            outputsCv[i] = get_cv() / Constraints.VOLT;
 
         }
 
-    	
+
     }
 
-    boolean firsttime = true;
 
-    
+
+
     public double get_cv() {
 
-        return octave + ((double)this.get_last_note()/12);
+        return octave + ((double) this.get_last_note() / 12);
     }
-    
-    public double get_gate () {
-    	for (int i = 0; i< 13;i++) {
-            if  (notes_playing[i]) {
+
+    public double get_gate() {
+        for (int i = 0; i < 13; i++) {
+            if (notes_playing[i]) {
                 return Constraints.VOLT;
             }
-         }
-    	return -Constraints.VOLT;
+        }
+        return -Constraints.VOLT;
     }
-    
-    
 
-    private void play_signal (double hz, boolean play) {
-		if (firsttime) {
-		    firsttime = false;
+
+    private void play_signal(double hz, boolean play) {
+        if (firsttime) {
+            firsttime = false;
             cur_hz = hz;
-		    return;
-		}
-		if (!play) {
+            return;
+        }
+        if (!play) {
             cur_hz = 0;
-			return;
-		}
+            return;
+        }
 
-		cur_hz=hz;
+        cur_hz = hz;
 
 
- }
+    }
 
-	
+
     public String update_ouput_signal() {
-    	update_playing();
+        update_playing();
 
         return "";
 
     }
-    private int get_last_note () {
-        long max= -1;
-        int index=-1;
-        for (int i = 0; i< 13;i++) {
+
+    private int get_last_note() {
+        long max = -1;
+        int index = -1;
+        for (int i = 0; i < 13; i++) {
             if (max < notes_timestamp[i]) {
-                max=notes_timestamp[i];
-                index=i;
+                max = notes_timestamp[i];
+                index = i;
             }
         }
         return index;
     }
 
-    
-    private void update_playing() {  
-       int i = get_last_note();
-       
-        if (notes[i] == true && ! notes_playing[i]) {
-          start_playing(i);
-          return;
-            }
 
+    private void update_playing() {
+        int i = get_last_note();
 
-          if (notes[i] == false && notes_playing[i]) {
-            stop_playing(i);
-              return;
-
-          }
-      }
-
-
-    
-	private void stop_playing(int i) {
-		notes_playing[i]=false;
-		System.out.println("stop"+i);
-		play_signal(notes_hz[i], false);
-
-		
-	}
-
-
-
-	private void start_playing(int i) {
-		stop_playing_all();
-		notes_playing[i]=true;
-		System.out.println("start"+i);
-		play_signal(notes_hz[i], true);
-		
-	}
-
-	private void stop_playing_all() {
-        for (int i = 0; i < 13; i++) {
-if (notes_playing[i]) {
-	stop_playing(i);
-	notes_playing[i]=false;
-}
+        if (notes[i] == true && !notes_playing[i]) {
+            start_playing(i);
+            return;
         }
-	}
 
- 
+
+        if (notes[i] == false && notes_playing[i]) {
+            stop_playing(i);
+            return;
+
+        }
+    }
+
+
+    private void stop_playing(int i) {
+        notes_playing[i] = false;
+        play_signal(notes_hz[i], false);
+
+
+    }
+
+
+    private void start_playing(int i) {
+        stop_playing_all();
+        notes_playing[i] = true;
+        play_signal(notes_hz[i], true);
+
+    }
+
+    private void stop_playing_all() {
+        for (int i = 0; i < 13; i++) {
+            if (notes_playing[i]) {
+                stop_playing(i);
+                notes_playing[i] = false;
+            }
+        }
+    }
+
+
     /////////////////////////////////////////////////////////////////:
     protected String print_frequencies() {
-        String res="";
+        String res = "";
         for (int i = 0; i < 13; i++) {
             if (notes[i] == true) {
-               double  result = (double) Math.round(((octave + (i/12d))*100d))/100d;
-                res+= notes_touch[i]+ " : " +this.notes_descr[i] + " : " + result + " V with octave=" + this.octave+"\n";
+                double result = (double) Math.round(((octave + (i / 12d)) * 100d)) / 100d;
+                res += notes_touch[i] + " : " + this.notes_descr[i] + " : " + result + " V with octave=" + this.octave + "\n";
             } else {
-                res+= notes_touch[i]+ " : " +this.notes_descr[i] +" : with octave=" + this.octave+"\n";
+                res += notes_touch[i] + " : " + this.notes_descr[i] + " : with octave=" + this.octave + "\n";
 
             }
         }
-         return  res;
+        return res;
     }
 
-    private String format_double(double d) {
-        return String.format("%.3f", d);
-    }
 
     private void compute_frequency(int index) {
         //		notes_hz[note]=  octave*0; //la formule...
@@ -195,13 +185,14 @@ if (notes_playing[i]) {
         return this.Vo * Math.pow(2, octave - 5) * Math.pow(2, index / 12.0);
 
     }
+
     private long get_timestamp() {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-    return timestamp.getTime();  
+        return timestamp.getTime();
     }
 
     protected void toggle_on(int note) {
-        this.notes_timestamp[note]=get_timestamp();
+        this.notes_timestamp[note] = get_timestamp();
         if (this.notes[note] == false) {
             this.notes[note] = true;
             this.compute_frequency(note);
@@ -213,7 +204,7 @@ if (notes_playing[i]) {
     }
 
     protected void toggle_off(int note) {
-    	this.notes_timestamp[note]=get_timestamp();
+        this.notes_timestamp[note] = get_timestamp();
 
         if (this.notes[note] == true) {
 
@@ -224,7 +215,6 @@ if (notes_playing[i]) {
 
         }
     }
-
 
 
     public void onpressOctaveUP() {
