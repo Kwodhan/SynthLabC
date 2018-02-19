59155d36199a53fda5203577a9e7f9f2911820f7
aferey <kwodhan@gmail.com>
2018-02-19 09:11:24 +0100

    add jar plugin

--- src/main/java/com/istic/App.java
+++ src/main/java/com/istic/App.java
@@ -13,6 +13,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        System.out.println();
         root = FXMLLoader.load(getClass().getResource("../../main.fxml"));
         scene = new Scene(root);
         stage.setResizable(false);
